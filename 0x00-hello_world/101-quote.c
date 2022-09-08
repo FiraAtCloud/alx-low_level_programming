61321031c190e72cd3acebeb2baf33bbaad1ce35
FiraAtCloud <firafiroo29@gmail.com>
2022-09-08 12:44:39 -0700

    final commit for c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int main(void) 
+/*
+ *main - function will print the text piece of art is useful - dora korpar 20...
+ *return - returns 0 it is success
+ */
+int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-return (1);  
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
