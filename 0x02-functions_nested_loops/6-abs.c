7937ea7f661f19e191c6936d5fb57f0c563145d1
FiraAtCloud <firafiroo29@gmail.com>
2022-09-13 06:17:00 -0700

    the files abs , print lastdigit , 24 hours

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - this function calculates the absolute value of an integer
+ * @n: the integer to be checked
+ * Return: this will return the absolute value of int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
