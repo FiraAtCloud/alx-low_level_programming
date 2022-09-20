cffc07933efb722adad41b4ef04853e21c8f983d
FiraAtCloud <firafiroo29@gmail.com>
2022-09-20 14:07:36 -0700

    files created and added README.md, main.h, 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 - this function will assign 98
+*to the variable pointed to by n
+*@n: this is the pointer
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this folder we will explore about ponters for the first time
