13ed6f218d4637be979955e6824a14b2dad3ec2c
FiraAtCloud <firafiroo29@gmail.com>
2022-09-24 09:36:54 -0700

    file edited and added 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,14 @@
 #include "main.h"
 /**
-*
-*/
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
 void _puts(char *str)
 {
-printf("%s\n", "I do not fear computers. I fear the lack of them - Isaac Asimov");
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
