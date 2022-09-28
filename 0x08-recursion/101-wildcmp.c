749851dc8e2cbc9f3459bfb04a12f90fcc0fc00c
FiraAtCloud <firafiroo29@gmail.com>
2022-09-28 09:28:08 -0700

    101-wildcmp.c updated and added

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,18 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 int bandersnatch(char *s1, char *s2);
-
 char *move(char *s2);
-
-
-
 /**
-
  * wildcmp - compares two strings recursively,
-
  * checking for wildcards expansion
-
  * @s1: first string to compare
-
  * @s2: second string to compare
-
  *
-
  * Return: 1 if the strings can be considered identical
-
  * otherwise 0
-
  */
-
 int wildcmp(char *s1, char *s2)
-  
 {
-  
   /**
-
    * this is going to be a sum of return values
    */
 int sum = 0;
